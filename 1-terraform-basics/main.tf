f6bd811ce5deabc71f953e40cc13144bfb92ff55
Kris Pigeau <kris.pigeau@gmail.com>
2023-03-30 08:53:51 -0400

    main.tf creates bootstapped ec2 webserver

--- 1-terraform-basics/main.tf
+++ 1-terraform-basics/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.60.0"
     }
   }
@@ -9,6 +9,24 @@ terraform {
 
 provider "aws" {
   # Configuration options
-  region = "us-east-1"
-  shared_credentials_file =["/home/kris/.aws/credentials"]
+  region                   = "us-east-1"
+  shared_credentials_files = ["/home/kris/.aws/credentials"]
+}
+resource "aws_instance" "VM-08" {
+  ami               = "ami-00c39f71452c08778"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name          = "kris_desktop"
+  security_groups   = ["demo-sg"]
+  tags              = { Name = "VM-08" }
+  user_data         = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install httpd -y
+    cd /var/www/html
+    echo "Hello from $(hostname -f)" \
+    > index.html
+    systemctl restart httpd
+    systemctl enable httpd
+    EOF
 }
