785a5bd87a98335615bbc1d7b07e47b38f05a191
Kris Pigeau <kris.pigeau@gmail.com>
2023-03-31 13:13:08 -0400

    added public RT and SN

--- 2-two-subnets/networking/net-main.tf
+++ 2-two-subnets/networking/net-main.tf
@@ -1,4 +1,4 @@
-# Create VPC
+# Make VPC
 
 resource "aws_vpc" "vpc-a" {
     cidr_block = "192.168.0.0/16"
@@ -7,7 +7,30 @@ resource "aws_vpc" "vpc-a" {
     tags = { Name = "vpc_a"}
 }
 
+# Make internet gateway
+
 resource "aws_internet_gateway" "igw-vpc-a" {
     vpc_id = aws_vpc.vpc-a.id
     tags    = { Name = "igw-vpc-a" }
+}
+
+# Make public routing table with default route pointing to igw
+
+resource "aws_route_table" "RT-public" {
+    vpc_id = aws_vpc.vpc-a.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw-vpc-a.id
+    }
+    tags = { Name = "public access routing table" }
+}
+
+# Make public subnet
+
+resource "aws_subnet" "SN-public-1" {
+    vpc_id = aws_vpc.vpc-a.id
+    cidr_block = "192.168.1.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "us-east-1a"
+    tags = { Name = "SN-public-1"}
 }
