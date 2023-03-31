77df83d6339e0be3f6e2ccce40607e8d85b4aa70
Kris Pigeau <kris.pigeau@gmail.com>
2023-03-31 13:02:28 -0400

    added make igw

--- 2-two-subnets/networking/net-main.tf
+++ 2-two-subnets/networking/net-main.tf
@@ -1,8 +1,13 @@
 # Create VPC
 
-resource "aws_vpc" "vpc_a" {
+resource "aws_vpc" "vpc-a" {
     cidr_block = "192.168.0.0/16"
     enable_dns_hostnames = true
     enable_dns_support = true
     tags = { Name = "vpc_a"}
+}
+
+resource "aws_internet_gateway" "igw-vpc-a" {
+    vpc_id = aws_vpc.vpc-a.id
+    tags    = { Name = "igw-vpc-a" }
 }
