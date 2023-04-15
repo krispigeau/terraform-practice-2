9bad23219b01493a417aedfcee637837630991ef
krispigeau <kris.pigeau@gmail.com>
2023-04-15 22:25:23 +0000

    modified bootstrap script

--- 4-two-modules/compute/apache.sh
+++ 4-two-modules/compute/apache.sh
@@ -2,7 +2,6 @@
 yum update -y
 yum install httpd -y
 cd /var/www/html/
-echo "<html><body><h1> VM-$(hostname -f) \
-</h1></body></html>" > index.html
+echo "<html><body><h1> VM-$(hostname -f) </h1></body></html>" > index.html
 systemctl restart httpd
 systemctl enable httpd
